e399d6d849464a3bc25bcc79af93cf855991952e
godrose <godrose@gmail.com>
2016-03-23 16:11:38 +0200

    Added tests.

---
+++ src/LogoFX.Client.Bootstrapping.Platform.NET45.Tests/BootstrapperTests.cs
@@ -0,0 +1,35 @@
+﻿using LogoFX.Client.Bootstrapping.Adapters.Unity;
+using NUnit.Framework;
+
+namespace LogoFX.Client.Bootstrapping.Platform.NET45.Tests
+{
+    [TestFixture]
+    class BootstrapperTests
+    {
+        [Test]
+        public void Initialization_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => new TestBootstrapper(new UnityContainerAdapter(),new BootstrapperCreationOptions
+            {
+                UseApplication = false
+            }));
+        }
+    }
+
+    class TestShellViewModel
+    {
+        
+    }
+
+    class TestBootstrapper : BootstrapperContainerBase<TestShellViewModel, UnityContainerAdapter>
+    {
+        public TestBootstrapper(UnityContainerAdapter iocContainerAdapter) : base(iocContainerAdapter)
+        {
+        }
+
+        public TestBootstrapper(UnityContainerAdapter iocContainerAdapter, BootstrapperCreationOptions creationOptions) : 
+            base(iocContainerAdapter, creationOptions)
+        {
+        }
+    }
+}
