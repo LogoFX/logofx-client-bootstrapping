ab142b8b349e89951f64229a4131ca9f72f0e23d
godrose <godrose@gmail.com>
2016-03-30 09:19:26 +0300

    Extracted the middleware code to the dedicated files.

---
+++ src/LogoFX.Client.Bootstrapping.Platform/src/BootstrapperContainerBase.Middleware.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Solid.Practices.Middleware;
+
+namespace LogoFX.Client.Bootstrapping
+{
+    public partial class BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter, TIocContainer>
+    {
+        private readonly
+            List<IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter, TIocContainer>>>
+            _middlewares =
+                new List<IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter, TIocContainer>>>();
+
+        /// <summary>
+        /// Uses the specified middleware.
+        /// </summary>
+        /// <param name="middleware">The middleware.</param>
+        /// <returns></returns>
+        public BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter, TIocContainer> Use(
+            IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter, TIocContainer>> middleware)
+        {
+            _middlewares.Add(middleware);
+            return this;
+        }
+    }
+
+    public partial class BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter>
+    {
+        private readonly List<IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter>>> _middlewares =
+            new List<IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter>>>();
+
+        /// <summary>
+        /// Uses the specified middleware.
+        /// </summary>
+        /// <param name="middleware">The middleware.</param>
+        /// <returns></returns>
+        public BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter> Use(
+            IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter>> middleware)
+        {
+            _middlewares.Add(middleware);
+            return this;
+        }
+    }
+}
--- src/LogoFX.Client.Bootstrapping.Platform/src/BootstrapperContainerBase.cs
+++ src/LogoFX.Client.Bootstrapping.Platform/src/BootstrapperContainerBase.cs
@@ -233,43 +233,5 @@ namespace LogoFX.Client.Bootstrapping
         {
             Dispatch.Current.InitializeDispatch();
         }
-    }
-
-    public partial class BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter, TIocContainer>
-    {
-        private readonly
-            List<IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter, TIocContainer>>>
-            _middlewares =
-                new List<IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter, TIocContainer>>>();
-
-        /// <summary>
-        /// Uses the specified middleware.
-        /// </summary>
-        /// <param name="middleware">The middleware.</param>
-        /// <returns></returns>
-        public BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter, TIocContainer> Use(
-            IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter, TIocContainer>> middleware)
-        {
-            _middlewares.Add(middleware);
-            return this;
-        }
-    }
-
-    public partial class BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter>
-    {
-        private readonly List<IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter>>> _middlewares =
-            new List<IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter>>>();
-
-        /// <summary>
-        /// Uses the specified middleware.
-        /// </summary>
-        /// <param name="middleware">The middleware.</param>
-        /// <returns></returns>
-        public BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter> Use(
-            IMiddleware<BootstrapperContainerBase<TRootViewModel, TIocContainerAdapter>> middleware)
-        {
-            _middlewares.Add(middleware);
-            return this;
-        }
-    }
+    }    
 }
