cc78f034e5a8f8e12278c6b5be937bace376d858
Gennady Verdel <godrose@gmail.com>
2017-07-15 23:47:12 +0300

    Moved the bootstrapping core source files to the dedicated folder

---
+++ src/LogoFX.Client.Bootstrapping/src/Middleware.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using LogoFX.Bootstrapping;
+using Solid.Bootstrapping;
+using Solid.Practices.Composition.Contracts;
+using Solid.Practices.Middleware;
+
+namespace LogoFX.Client.Bootstrapping
+{
+    /// <summary>
+    /// Registers automagically the application's view models in the transient lifestyle.
+    /// </summary>
+    public class RegisterViewModelsMiddleware :
+        IMiddleware<IBootstrapperWithContainerRegistrator>
+    {
+        private readonly IEnumerable<Type> _excludedTypes;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegisterViewModelsMiddleware"/> class.
+        /// </summary>
+        /// <param name="excludedTypes">The type of the root object.</param>
+        public RegisterViewModelsMiddleware(IEnumerable<Type> excludedTypes)
+        {
+            _excludedTypes = excludedTypes;
+        }
+
+        /// <summary>
+        /// Applies the middleware on the specified object.
+        /// </summary>
+        /// <param name="object">The object.</param>
+        /// <returns></returns>
+        public IBootstrapperWithContainerRegistrator Apply(
+            IBootstrapperWithContainerRegistrator @object)
+        {
+            var internalMiddleware = new RegisterViewModelsMiddleware<IBootstrapperWithContainerRegistrator>(_excludedTypes);
+            return internalMiddleware.Apply(@object);
+        }
+    }
+
+    /// <summary>
+    /// Registers automagically the application's view models in the transient lifestyle.
+    /// </summary>
+    public class RegisterViewModelsMiddleware<TBootstrapper> :
+        IMiddleware<TBootstrapper> 
+        where TBootstrapper : class, IHaveContainerRegistrator, IAssemblySourceProvider
+    {
+        private readonly IEnumerable<Type> _excludedTypes;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegisterViewModelsMiddleware"/> class.
+        /// </summary>
+        /// <param name="excludedTypes">The type of the root object.</param>
+        public RegisterViewModelsMiddleware(IEnumerable<Type> excludedTypes)
+        {
+            _excludedTypes = excludedTypes;
+        }
+
+        /// <summary>
+        /// Applies the middleware on the specified object.
+        /// </summary>
+        /// <param name="object">The object.</param>
+        /// <returns></returns>
+        public TBootstrapper Apply(
+            TBootstrapper @object)
+        {
+            @object.Registrator.RegisterViewModels(@object.Assemblies, _excludedTypes);
+            return @object;
+        }
+    }
+}
