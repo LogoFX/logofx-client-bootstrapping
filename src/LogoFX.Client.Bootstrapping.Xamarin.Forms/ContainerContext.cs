6785928b9299124da8378ba33b62990462b4bbc8
Gennady Verdel <godrose@gmail.com>
2018-02-27 12:18:51 +0200

    #11 Added support for iOS and Android platforms

---
+++ src/LogoFX.Client.Bootstrapping.Xamarin.Forms/ContainerContext.cs
@@ -0,0 +1,20 @@
+﻿using LogoFX.Client.Bootstrapping.Adapters.Contracts;
+using Solid.Practices.IoC;
+
+namespace LogoFX.Client.Bootstrapping.Xamarin.Forms
+{
+    public static class ContainerContext
+    {
+        private static object _instance;
+
+        public static void SetAdapter<TContainerAdapter>(TContainerAdapter containerAdapter)
+            where TContainerAdapter : IDependencyRegistrator, IDependencyResolver, IBootstrapperAdapter => _instance = containerAdapter;
+
+        public static IDependencyRegistrator Registrator => (IDependencyRegistrator)_instance;
+
+        public static IDependencyResolver Resolver => (IDependencyResolver)_instance;
+
+        public static IBootstrapperAdapter Adapter => (IBootstrapperAdapter)_instance;
+    }
+     
+}
---
+++ src/LogoFX.Client.Bootstrapping.Xamarin.Forms/RegisterRootViewModelMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using Solid.Bootstrapping;
+using Solid.Practices.Middleware;
+
+namespace LogoFX.Client.Bootstrapping.Xamarin.Forms
+{
+    public class RegisterRootViewModelMiddleware<TBootstrapper, TRootViewModel> : IMiddleware<TBootstrapper>
+        where TBootstrapper : class, IHaveRegistrator
+        where TRootViewModel : class
+    {
+        public TBootstrapper Apply(TBootstrapper @object)
+        {
+            @object.Registrator.RegisterSingleton<TRootViewModel>();
+            return @object;
+        }
+    }
+
+}
