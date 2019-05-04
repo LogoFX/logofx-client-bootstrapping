8837dae338df8932e9e516a8f578b8cc38a5b829
Gennady Verdel <godrose@gmail.com>
2019-05-04 17:14:42 +0300

    chore: added comments #17

--- src/LogoFX.Client.Bootstrapping.Platform/src/MiddlewareDecorator.cs
+++ src/LogoFX.Client.Bootstrapping.Platform/src/MiddlewareDecorator.cs
@@ -7,6 +7,8 @@ namespace LogoFX.Client.Bootstrapping
     //TODO: Move to Solid.Extensibility|Bootstrapping - pay attention to dependency on IoC
     //Add Use<T> at the IExtensible level
     //Add IMiddleware interface as marker interface for easier automagical registration
+    //This is actually needed to support middlewares registration into the container.
+    //This functionality is not needed otherwise.
     class MiddlewareDecorator<TExtensible, TMiddleware> : IMiddleware<TExtensible>
         where TExtensible : class, IExtensible<TExtensible>
         where TMiddleware : class, IMiddleware<TExtensible>
