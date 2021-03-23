6d8d82a876778ddbf3fd3164c127b640a6c140cc
Gennady Verdel <godrose@gmail.com>
2021-03-23 14:31:11 +0200

    chore: minor

--- devops/test/test.bat
+++ devops/test/test.bat
@@ -1,4 +1,4 @@
-rem provide more generic way for drives case
+rem provide more generic way for non-global packages case
 SET current_dir=%cd%
 echo %current_dir%
 %UserProfile%/.nuget/packages/xunit.runner.console/2.4.1/tools/net461/xunit.console.exe ../../src/LogoFX.Client.Bootstrapping.Platform.NETFramework.Tests/bin/Release/LogoFX.Client.Bootstrapping.Platform.NETFramework.Tests.dll
