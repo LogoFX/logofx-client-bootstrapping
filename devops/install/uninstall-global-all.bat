adf933e1c00ceb38b185400a0a2cc08594e2d800
Gennady Verdel <godrose@gmail.com>
2021-04-04 17:45:50 +0300

    devops: added means for package creation and storage #24

--- devops/install/uninstall-global-all.bat
+++ devops/install/uninstall-global-all.bat
@@ -1,4 +1,4 @@
-call uninstall-global-single LogoFX.Client.Bootstrapping.Core
-call uninstall-global-single LogoFX.Client.Bootstrapping
-call uninstall-global-single LogoFX.Client.Bootstrapping.Testing
-call uninstall-global-single LogoFX.Client.Bootstrapping.Xamarin.Forms
+call uninstall-global-single LogoFX.Client.Bootstrapping.Core %1
+call uninstall-global-single LogoFX.Client.Bootstrapping %1
+call uninstall-global-single LogoFX.Client.Bootstrapping.Testing %1
+call uninstall-global-single LogoFX.Client.Bootstrapping.Xamarin.Forms %1
