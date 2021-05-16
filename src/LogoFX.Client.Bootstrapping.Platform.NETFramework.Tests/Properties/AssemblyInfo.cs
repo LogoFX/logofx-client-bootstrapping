4378ef6ed9cc726b620578bfe37ebcdff7219af7
Gennady Verdel <godrose@gmail.com>
2021-05-16 11:58:49 +0300

    chore: bump deps

--- src/LogoFX.Client.Bootstrapping.Platform.NETFramework.Tests/Properties/AssemblyInfo.cs
+++ src/LogoFX.Client.Bootstrapping.Platform.NETFramework.Tests/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LogoFX.Client.Bootstrapping.Platform.NETFramework.Tests")]
-[assembly: AssemblyCopyright("Copyright �  2016")]
+[assembly: AssemblyCopyright("Copyright ?  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
